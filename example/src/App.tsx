cdfaa4aa66739780741968b893ee408838ae9d2a
Sim Gill <simgill89@gmail.com>
2023-04-22 18:33:03 -0700

    initial

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-designlibrary';
+import { multiply, TestButton, TextView } from 'react-native-designlibrary';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
@@ -13,6 +13,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
+      <TestButton />
+      <TextView/>
     </View>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,13 @@
-import { NativeModules, Platform } from 'react-native';
+import React from 'react';
+import {
+  NativeModules,
+  Platform,
+  View,
+  Button,
+  StyleProp,
+  ViewStyle,
+  Text,
+} from 'react-native';
 
 const LINKING_ERROR =
   `The package 'react-native-designlibrary' doesn't seem to be linked. Make sure: \n\n` +
@@ -17,6 +26,31 @@ const Designlibrary = NativeModules.Designlibrary
       }
     );
 
+export function sim(a: number) {
+  return a;
+}
 export function multiply(a: number, b: number): Promise<number> {
   return Designlibrary.multiply(a, b);
 }
+
+interface Props {
+  color: string;
+  action: () => void;
+  style?: StyleProp<ViewStyle>;
+}
+
+export const TestButton: React.FC<Props> = ({ color, action, style }) => {
+  return (
+    <View style={[{ backgroundColor: color }, style]}>
+      <Button title={'Press'} onPress={action} />
+    </View>
+  );
+};
+
+export const TextView: React.FC<null> = () => {
+  return (
+    <View style={{ height: 400, width: 300, backgroundColor: 'grey' }}>
+      <Text>Test Component</Text>
+    </View>
+  );
+};
